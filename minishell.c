4a8fb9c86f718cc6eaafc3590ffb7932a1ad2735
Omar Idrissi yazami <oidrissi@e2r2p3.1337.ma>
2021-11-02 21:56:11 +0100

    launch

--- minishell.c
+++ minishell.c
@@ -91,15 +91,6 @@ int		ft_countwords(char *s, char c)
 	return (words);
 }
 
-// char *join_between_quotes(char *s)
-// {
-// 	int i = 0;
-// 	while (s[i])
-// 	{
-// 		if (s[i] == )
-// 	}
-// }
-
 char **ft_split_with_quotes(char *s, char c)
 {
 	char **ret;
@@ -130,33 +121,6 @@ char **ft_split_with_quotes(char *s, char c)
 	}
 	return NULL;
 }
-
-// static	size_t ft_db_quotes_len(char *s)
-// {
-// 	size_t i;
-// 	size_t count;
-
-// 	i = 0;
-// 	count = 0;
-// 	while (s[i])
-// 	{
-// 		if (s[i] == '\"' && s[i + 1] != '\0')
-// 		{	
-// 			i++;
-// 			while (s[i] != '\"')
-// 			{
-// 				i++;
-// 				if (s[i] == ' ')
-// 					i++;
-// 			}
-// 			count += 1;
-// 		}
-// 		else if (s[i] == ' ' && s[i + 1] != '\"')
-// 			count += 1;
-// 		i++;
-// 	}
-// 	return (count);
-// }
 
 static	size_t	ft_word_len(char *s, char c)
 {
@@ -195,62 +159,6 @@ static	char	**ft_free(char **f, size_t j)
 	return (NULL);
 }
 
-// char	*in_quotes(char *s)
-// {
-// 	int i = 0;
-// 	int start;
-// 	char *new;
-// 	char *new_db;
-
-// 	new = malloc(sizeof(char ) * ft_strlen(s) + 1);
-// 	while (s[i])
-// 	{
-// 		if (s[i] == '\'')
-// 		{
-// 			start = i;
-// 			i += 1;
-// 			// if (s[i] == '\'')
-// 				//special case
-// 			while (s[i] != '\'')
-// 				i++;
-// 			new = ft_substring(s, start, i);
-// 			new[i] = '\0';
-// 			// token->single_quotes += 1
-// 			return (new);
-// 		}
-// 		else if (s[i] == '\"')
-// 		{
-// 			i +=1;
-// 			while (s[i] != '\'')
-// 				i++;
-// 			return (s);
-// 			// token->db_quotes += 1;
-// 		}
-// 		i++;
-// 	}
-// 	return NULL;
-// }
-
-// char 	**ft_tokenizer(char *s)
-// {
-// 	int i;
-// 	int j;
-// 	int **new;
-
-// 	i = 0;
-// 	j = 0;
-// 	new = malloc(sizeof(char *) * (ft_strlen(s)));
-// 	while (s[i])
-// 	{
-// 		if (out_of_quotes(s) != NULL)
-// 			*new[i] = ;
-// 		// printf("%s", s);
-// 		// new[i] = ft_substr(s, 0, j);
-// 		i++;
-// 	}
-// 	return NULL;
-// }
-
 char	**ft_split(char *s, char c)
 {
 	size_t		i;
@@ -262,26 +170,18 @@ char	**ft_split(char *s, char c)
 	i = 0;
 	j = 0;
 	l_count = 0;
-	// if (is_quotes(s))
-	// countw = ft_db_quotes_len(s);
 	countw = ft_countwords((char *)s, c);
-	printf("Number of words %zu: \n", countw);
 	p = (char **)malloc(sizeof(char *) * (countw + 1));
-	int insgl = 0;
-	int indbl = 0;
-	int last = 0;
 	i = 0;
 	while (i < countw)
 	{
 		while (s[j] == c)
 			j++;
 		l_count = ft_word_len((char *)s + j, c);
-		// printf("Length of words %zu: \n", l_count);
 		p[i] = ft_substring(s, j, l_count);
 		if (!p)
 			return (ft_free(p, i));
 		i++;
-		j += l_count + 1;
 	}
 	p[countw] = NULL;
 	return (p);
@@ -363,38 +263,14 @@ int main(int ac, char **av, char **envp)
     while (1)
     {
         ret_read = readline(prompt);
-		// *tokens = in_quotes(ret_read);
-		// printf("%s", *tokens);
         if (!ret_read)
             return (0) ;
-		// tokens = ft_tokenizer(ret_read);
-        tokens = ft_split(ret_read, ' ');
+        tokens = ft_split(ret_read, '|');
         while (tokens[i])
         {
-			// Parsing tokens
-			int j = 0;
-			// is_quotes(tokens, i, j);
-			is_redir(tokens, i, j);
-            if (tokens[i][j] == '|')
-            {
-				j += 1;
-                pipe = 1; // need to execute the pipe everytime incremented set by one
-				if (pipe == 1 && tokens[i][j] != '|')
-					pipe = 0;
-				else
-					break ;
-                printf("pipe found\n");
-            }
-			// if (is_builtin(tokens[i]))
-			// {
-			// 	continue ;
-			// }
-            // // free(tokens[i]);
-			else
-            	printf("Token %d is %s |\n", i, tokens[i]);
+            printf("Token %d is %s |\n", i, tokens[i]);
             i++;
         }
-        i = 0;
         free(tokens);
     }
     free(ret_read);
--- tokenizer.c
+++ tokenizer.c
@@ -6,12 +6,16 @@
 /*   By: oidrissi <oidrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 16:16:50 by oidrissi          #+#    #+#             */
-/*   Updated: 2021/11/01 13:38:05 by oidrissi         ###   ########.fr       */
+/*   Updated: 2021/11/02 21:55:28 by oidrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "minishell.h"
 #include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
 
 // Order tokens in 2d array
 // Enter value : char *readline_result;
@@ -25,8 +29,8 @@ char    *in_quotes(char *s)
     while (s[i])
     {
         if (s[i] == '\"')
-        {
-			t_sh->quotes = 1;
+        // {
+			// t_sh->quotes = 1;
             return (&s[i]);
         else if (s[i] == '\'')
             return (&s[i]);
@@ -35,6 +39,186 @@ char    *in_quotes(char *s)
     return (NULL);
 }
 
+int    length_quote(char *s)
+{
+    int i;
+    char *v;
+    
+    v = in_quotes(s);
+    i = 1;
+    while (v[i])
+    {
+        if (v[i] == '\"')
+            return (i);
+        i++;
+    }
+    return (0);
+}
+
+int ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char    *trim_quote(char *s)
+{
+    unsigned int i;
+    char *v;
+
+    v = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+    s = in_quotes(s);
+    i = 0;
+    while (i <= length_quote(s))
+    {
+        v[i] = s[i];
+        i++;
+    }
+    v[i] = '\0';
+    return (v);
+}
+
+// char **split()
+// {
+    
+// }
+
+
+
+int count_cmds(char *s)
+{
+    int i;
+    int j;
+    int word;
+    
+    word = 1;
+    i = 0;
+    j = 0;
+    // if (s[i])
+    while (s[i])
+    {
+        if (s[i] == '|' && s[i + 1])
+        {
+            while (j != i)
+            {
+                if (s[j] != ' ')
+                    break;
+                j++;
+            }
+            if (j == i)
+                return -1;
+            // while (j != i)
+            // {
+                
+            //     j++;   
+            // }
+            word++;
+        }   
+        i++;
+    }
+    return (word);
+}
+
+static	char	**ft_free(char **f, size_t j)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < j)
+		free(f[i++]);
+	free(f);
+	return (NULL);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char			*p;
+	int				i;
+	unsigned int	bigg;
+
+	i = 0;
+	bigg = start;
+	if (!s || start > ft_strlen(s))
+	{
+		p = malloc(1 * sizeof(char));
+		p[0] = '\0';
+		return (p);
+	}
+	p = malloc((len + 1) * sizeof(char));
+	if (p == 0)
+		return (NULL);
+	while ((start < (unsigned int)len + bigg && s[start]))
+	{
+		p[i] = s[start];
+		start++;
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+int ft_cmd_len(char *s, char c)
+{
+    int i;
+
+    i = 0;
+    while (s[i] && s[i] != c)
+        i++;
+    return (i);
+}
+
+int is_between_quotes(char *s)
+{
+    int i;
+
+    i = 0;
+	j = 0;
+    while (s[i])
+    {
+        if (s[i] == '\"')
+		{
+			ft_substr(s, i, )
+			i++;
+			while (s[i])
+				i++;
+		}
+		i++;   
+    }
+}
+
+char **commands(char *s)
+{
+    int i;
+    int j;
+    char **cmds;
+    int nb_elems;
+    int cmd_len;
+
+    i = 0;
+    j = 0;
+    nb_elems = count_cmds(s);
+    if (nb_elems == -1)
+        return NULL;
+    cmds = (char **)malloc(sizeof(char *) * (nb_elems + 1));
+    printf("%d\n", nb_elems);
+    while (i < nb_elems)
+    {
+        while (s[j] == '|' && )
+            j++;
+        cmd_len = ft_cmd_len((char *)s + j, '|');
+        cmds[i] = ft_substr(s, j, cmd_len);
+        if  (!cmds)
+            return (ft_free(cmds, i));
+        i++;
+        j += cmd_len + 1;
+    }
+    cmds[nb_elems] = NULL;
+    return (cmds);
+}
 
 
 // char    **ft_tokenizer(char *rdline_res)
@@ -51,9 +235,32 @@ char    *in_quotes(char *s)
 //     }
 // }
 
+// int check_error(char *s)
+
 int main(int ac, char **av)
 {
-    char str[] = "My name is \"hello\" World";
-    char *s = in_quotes(str);
-    printf("%s\n", s);
+    char *ret;
+    char *prompt;
+    char **tokens;
+    int		i;
+    
+    prompt = ">";
+    while (1)
+    {
+	    i = 0;
+        ret = readline(prompt);
+        if (*ret)
+            add_history(ret);
+        tokens = commands(ret);
+        if (!tokens)
+        {
+            write(1, "error\n", 6);
+            continue ;
+        }
+        while (tokens[i])
+        {
+            printf("Token is: %s\n", tokens[i]);
+            i++;
+        }
+    }
 }
